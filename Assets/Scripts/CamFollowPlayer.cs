e7d654a06a2cf4f4ee4ba50ae0097253a14a9c3a
TapJc <112350012+TapJc@users.noreply.github.com>
2022-09-08 15:36:11 -0500

    Added Camera and Player

---
+++ Assets/Scripts/CamFollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Brian Sida
+ * Project 1
+ * Description: Camera follows player
+*/
+public class CamFollowPlayer : MonoBehaviour
+{
+    // drag the player onto this reference in the Inspector
+    public GameObject player;
+
+    public Vector3 offset = new Vector3(0, 4, -8);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Brian Sida
+ * Project 1
+ * Description: Controls player movement
+*/
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    public float forwardInput;
+    public float horizontalInput;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 tempV = rb.velocity;
+
+        tempV.x = horizontalInput * speed;
+        tempV.z = forwardInput * speed;
+
+        rb.velocity = tempV;
+
+        tempV.y = 0;
+        transform.LookAt(tempV.normalized + transform.position);
+    }
+}
