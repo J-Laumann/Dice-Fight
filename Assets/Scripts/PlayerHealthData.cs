51f4a6354937d45203cfc8559505306a54e10ff8
Kmann725 <khmanning@mail.bradley.edu>
2022-09-14 17:57:40 -0500

    Added player health scriptable object

---
+++ Assets/Scripts/PlayerHealthData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player Health Data", menuName = "Player Health Data", order = 1)]
+public class PlayerHealthData : ScriptableObject
+{
+    public int health;
+}
