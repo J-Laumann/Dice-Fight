9d0ed473c47632fbbad63445464f428caf80c14d
Kyron Larenz Patterson <bluekeshon79@gmail.com>
2022-09-14 16:58:00 -0500

    Added code to handle changing between scenes when an enemy is hit in the overworld.

---
+++ Assets/Scripts/EnterBattleScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterBattleScript : MonoBehaviour
+{
+    TransitionHandler trans;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = FindObjectOfType<TransitionHandler>().GetComponent<TransitionHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            trans.SlideIn();
+            StartCoroutine("TriggerBattle");
+        }
+    }
+
+    private IEnumerator TriggerBattle()
+    {
+        yield return new WaitForSeconds(1.5f);
+
+        trans.SlideOut();
+
+        SceneManager.LoadScene("FightScene");
+
+    }    
+}
---
+++ Assets/Scripts/TransitionHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionHandler : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Grabs the animator from the Transition Image.
+        anim = GetComponentInChildren<Animator>();
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    /// <summary>
+    /// Plays the Slide In transition.
+    /// </summary>
+    public void SlideIn()
+    {
+        anim.Play("Slide_In");
+    }
+
+    /// <summary>
+    /// Plays the Slide out transition.
+    /// </summary>
+    public void SlideOut()
+    {
+        anim.Play("Slide_Out");
+    }
+}
