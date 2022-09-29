a09401a37b918bfe5ee9e5994eb669e9af97c723
TapJc <112350012+TapJc@users.noreply.github.com>
2022-09-28 22:10:50 -0500

    Added Overworld Tutorial

---
+++ Assets/Scripts/TutorialTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    public Tutorial dialogue;
+
+    public void TriggerTutorial()
+    {
+        // WIP
+        if (PlayerData.souls != 0)
+        {
+            FindObjectOfType<TutorialUIManager>().OverWorldTutorial(dialogue);
+        }
+        else
+        {
+            FindObjectOfType<TutorialUIManager>().BattleTutorial(dialogue);
+        }
+    }
+}
---
+++ Assets/Scripts/TutorialUIManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialUIManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text tutorialText;
+
+    private Queue<string> sentences;
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void OverWorldTutorial(Tutorial tutorial)
+    {
+        nameText.text = tutorial.name;
+
+        sentences.Clear();
+
+        // looks through string array and adds news sentences to the queue
+        foreach (string sentence in tutorial.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextTutorial();
+    }
+
+    public void BattleTutorial(Tutorial tutorial)
+    {
+        nameText.text = tutorial.name;
+
+        sentences.Clear();
+
+        // looks through string array and adds news sentences to the queue
+        foreach (string sentence in tutorial.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextTutorial();
+    }
+
+    public void DisplayNextTutorial()
+    {
+        if (sentences.Count == 0)
+        {
+            EndTutorial();
+            return;
+        }
+
+        // If queue isn't empty, print dequeue the next tutorial
+        string tutorial = sentences.Dequeue();
+        tutorialText.text = tutorial;
+    }
+
+    void EndTutorial()
+    {
+        Debug.Log("End of tutorial.");
+    }
+}
